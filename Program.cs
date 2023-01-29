953f76f3268b04f0cb5475651dc64f1fbd853ddf
Anton Ilin <ilinas369@gmail.com>
2023-01-29 14:44:46 +0400

    добавлен метод TransformArray

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] arrayFirst = { "Hello", "2", "world", ":-)" };
+string[] arraySecond = TransformArray(arrayFirst);
+PrintArray(arraySecond);
+
+
+string[] TransformArray(string[] array)
+{
+    int LengthNewArray = 0;
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            LengthNewArray++;
+        }
+    }
+
+    string[] newArray = new string[LengthNewArray];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+
+    return newArray;
+}
